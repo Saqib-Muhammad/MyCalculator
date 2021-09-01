1e4c626d4f24f6494426d0fd15b97fa87668f63f
Muhammad Saqib <se.saqib1@gmail.com>
2021-09-01 12:35:02 +0500

    Initial commit

---
+++ app/src/main/java/com/muhammadsaqib/mycalculator/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.muhammadsaqib.mycalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        val viewModel = ViewModelProviders.of(this).get(CalculatorViewModel::class.java)
+        val viewModel = ViewModelProviders.of(this).get(BigDecimalViewModel::class.java)
+
+        // create or call the static of() of the viewModelProviders class to get a ViewModelProvider and then you call its get() to get the ViewModel type you want.
+
+        //  the code to start observing the ViewModel's LiveData objects is simple.
+        viewModel.stringResult.observe(this, Observer<String> {stringResult -> result.setText(stringResult)})
+        viewModel.stringNewNumber.observe(this, Observer<String> {stringNumber -> newNumber.setText(stringNumber)})
+        viewModel.stringOperation.observe(this, Observer<String> {stringOperation -> operation.text = stringOperation})
+
+        val listener = View.OnClickListener { v ->
+            viewModel.digitPressed((v as Button).text.toString())
+        }
+        button0.setOnClickListener(listener)
+        button1.setOnClickListener(listener)
+        button2.setOnClickListener(listener)
+        button3.setOnClickListener(listener)
+        button4.setOnClickListener(listener)
+        button5.setOnClickListener(listener)
+        button6.setOnClickListener(listener)
+        button7.setOnClickListener(listener)
+        button8.setOnClickListener(listener)
+        button9.setOnClickListener(listener)
+        buttonDot.setOnClickListener(listener)
+
+        val opListener = View.OnClickListener { v ->
+            viewModel.operandPressed((v as Button).text.toString())
+        }
+
+        buttonEqual.setOnClickListener(opListener)
+        buttonDivide.setOnClickListener(opListener)
+        buttonMultiply.setOnClickListener(opListener)
+        buttonMinus.setOnClickListener(opListener)
+        buttonPlus.setOnClickListener(opListener)
+
+        buttonNeg.setOnClickListener {
+            viewModel.negPressed()
+        }
+    }
+}
